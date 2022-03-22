afd0447649f4e3130ceff382512add72f0221fcf
hedwig100 <pentagon7951@outlook.jp>
2022-03-22 22:26:02 +0900

    [change] update zeta,mebius transform

--- math/zeta.cpp
+++ math/zeta.cpp
@@ -2,22 +2,41 @@
 #include <vector>
 
 // a <= b <=> a <= b
-// g[x] = \sum_{ i <= x } f[i]
-template <class T>
-void zeta(std::vector<T> &f) {
-    int sz = (int)f.size();
-    for (int x = 1; x < sz; x++) {
-        f[x] += f[x - 1];
-    }
-}
+// g[x] = \\sum_{ i <= x } f[i]
+template <class R>
+struct ZetaOrder {
+    // TODO:verify
+  public:
+    ZetaOrder() {}
 
-template <class T>
-void mebius(std::vector<T> &f) {
-    int sz = (int)f.size();
-    for (int x = sz - 1; x >= 1; x--) {
-        f[x] -= f[x - 1];
+    void zeta(std::vector<R> &f) {
+        int sz = (int)f.size();
+        for (int x = 1; x < sz; x++) {
+            f[x] += f[x - 1];
+        }
     }
-}
+
+    void mebius(std::vector<R> &f) {
+        int sz = (int)f.size();
+        for (int x = sz - 1; x >= 1; x--) {
+            f[x] -= f[x - 1];
+        }
+    }
+
+    std::vector<R> convolve(std::vector<R> f, std::vector<R> g) {
+        int sz = std::max((int)f.size(), (int)g.size());
+        f.resize(sz, 0);
+        g.resize(sz, 0);
+        zeta(f);
+        zeta(g);
+        std::vector<R> h(sz);
+        for (int i = 0; i < sz; i++) {
+            h[i] = f[i] * g[i];
+        }
+        mebius(h);
+        return h;
+    }
+};
 
 // min_pow2 returns minimum power of 2 larger than x (x <= 2^i)
 // and i (pair{i,2^i}).
@@ -36,69 +55,134 @@ std::pair<int, T> min_pow2(T x) {
 // S <= T <=> S \subset T
 // g[T] = \sum_{ S \subset T } f[S]
 template <class R>
-void zeta_set(std::vector<R> &f) {
-    auto [d, sz] = min_pow2((int)f.size());
-    f.resize(sz, (R)0);
-    for (int i = 0; i < d; i++) {
-        for (int T = 0; T < sz; T++) {
-            if (T & (1 << i))
-                f[T] += f[T ^ (1 << i)];
+struct ZetaSubset {
+    // TODO:verify
+  private:
+    // min_pow2 returns minimum power of 2 larger than x (x <= 2^i)
+    // and i (pair{i,2^i}).
+    // x must be more than 0
+    std::pair<int, int> min_pow2(int x) {
+        int i   = 0;
+        int ret = 1;
+        while (x > ret) {
+            i++;
+            ret <<= 1;
         }
+        return std::make_pair(i, ret);
     }
-}
 
-template <class R>
-void mebius_set(std::vector<R> &f) {
-    auto [d, sz] = min_pow2((int)f.size());
-    f.resize(sz, (R)0);
-    for (int i = 0; i < d; i++) {
-        for (int T = 0; T < sz; T++) {
-            if (T & (1 << i))
-                f[T] -= f[T ^ (1 << i)];
+  public:
+    ZetaSubset() {}
+
+    void zeta(std::vector<R> &f) {
+        auto [d, sz] = min_pow2((int)f.size());
+        f.resize(sz, (R)0);
+        for (int i = 0; i < d; i++) {
+            for (int T = 0; T < sz; T++) {
+                if (T & (1 << i))
+                    f[T] += f[T ^ (1 << i)];
+            }
         }
     }
-}
+
+    void mebius(std::vector<R> &f) {
+        auto [d, sz] = min_pow2((int)f.size());
+        f.resize(sz, (R)0);
+        for (int i = 0; i < d; i++) {
+            for (int T = 0; T < sz; T++) {
+                if (T & (1 << i))
+                    f[T] -= f[T ^ (1 << i)];
+            }
+        }
+    }
+
+    std::vector<R> convolve(std::vector<R> f, std::vector<R> g) {
+        int sz = std::max((int)f.size(), (int)g.size());
+        f.resize(sz, 0);
+        g.resize(sz, 0);
+        zeta(f);
+        zeta(g);
+        std::vector<R> h(sz);
+        for (int i = 0; i < sz; i++) {
+            h[i] = f[i] * g[i];
+        }
+        mebius(h);
+        return h;
+    }
+};
 
 // a <= b <=> b | a
 // g[x] = \sum_{ x | i } f[i]
-template <class T>
-void zeta_div(std::vector<T> &f) {
-    int sz = (int)f.size();
-    for (int x = 1; x < sz; x++) {
-        for (int i = 2 * x; i < sz; i += x) {
-            f[x] += f[i];
+template <class R>
+struct ZetaDiv {
+    // TODO: O(nloglogn) zeta transform
+  private:
+    // min_pow2 returns minimum power of 2 larger than x (x <= 2^i)
+    // and i (pair{i,2^i}).
+    // x must be more than 0
+    std::pair<int, int> min_pow2(int x) {
+        int i   = 0;
+        int ret = 1;
+        while (x > ret) {
+            i++;
+            ret <<= 1;
         }
+        return std::make_pair(i, ret);
     }
-}
 
-template <class T>
-void mebius_div(std::vector<T> &f) {
-    int sz = (int)f.size();
-    for (int x = sz - 1; x >= 1; x--) {
-        for (int i = 2 * x; i < sz; i += x) {
-            f[x] -= f[i];
+  public:
+    ZetaDiv() {}
+
+    void zeta(std::vector<R> &f) {
+        int sz = (int)f.size();
+        for (int x = 1; x < sz; x++) {
+            for (int i = 2 * x; i < sz; i += x) {
+                f[x] += f[i];
+            }
         }
     }
-}
+
+    void mebius(std::vector<R> &f) {
+        int sz = (int)f.size();
+        for (int x = sz - 1; x >= 1; x--) {
+            for (int i = 2 * x; i < sz; i += x) {
+                f[x] -= f[i];
+            }
+        }
+    }
+
+    std::vector<R> convolve(std::vector<R> f, std::vector<R> g) {
+        int sz = std::min((int)f.size(), (int)g.size());
+        zeta(f);
+        zeta(g);
+        std::vector<R> h(sz);
+        for (int i = 0; i < sz; i++) {
+            h[i] = f[i] * g[i];
+        }
+        mebius(h);
+        return h;
+    }
+};
 
 int sample() {
     std::vector<int> f = {0, 1, 3, 24, 4, 2, 4, 4, 2, 1};
 
     // a <= b
+    ZetaOrder<int> zt1;
     printf("before\n");
     for (int i = 0; i < (int)f.size(); i++) {
         printf("%d ", f[i]);
     }
     printf("\n");
 
-    zeta_div(f);
+    zt1.zeta(f);
     printf("transform\n");
     for (int i = 0; i < (int)f.size(); i++) {
         printf("%d ", f[i]);
     }
     printf("\n");
 
-    mebius_div(f);
+    zt1.mebius(f);
     printf("return\n");
     for (int i = 0; i < (int)f.size(); i++) {
         printf("%d ", f[i]);
@@ -106,20 +190,21 @@ int sample() {
     printf("\n");
 
     // S \subset T
+    ZetaSubset<int> zt2;
     printf("before\n");
     for (int i = 0; i < (int)f.size(); i++) {
         printf("%d ", f[i]);
     }
     printf("\n");
 
-    zeta_set(f);
+    zt2.zeta(f);
     printf("transform\n");
     for (int i = 0; i < (int)f.size(); i++) {
         printf("%d ", f[i]);
     }
     printf("\n");
 
-    mebius_set(f);
+    zt2.mebius(f);
     printf("return\n");
     for (int i = 0; i < (int)f.size(); i++) {
         printf("%d ", f[i]);
@@ -127,20 +212,21 @@ int sample() {
     printf("\n");
 
     // b | a
+    ZetaDiv<int> zt3;
     printf("before\n");
     for (int i = 0; i < (int)f.size(); i++) {
         printf("%d ", f[i]);
     }
     printf("\n");
 
-    zeta_div(f);
+    zt3.zeta(f);
     printf("transform\n");
     for (int i = 0; i < (int)f.size(); i++) {
         printf("%d ", f[i]);
     }
     printf("\n");
 
-    mebius_div(f);
+    zt3.mebius(f);
     printf("return\n");
     for (int i = 0; i < (int)f.size(); i++) {
         printf("%d ", f[i]);
@@ -216,16 +302,9 @@ int main() {
     for (int i = 1; i <= W; i++) {
         g[i] = W - i;
     }
-    zeta_div(f);
-    zeta_div(g);
 
-    int sz = std::min((int)f.size(), (int)g.size());
-    std::vector<mint> h(sz);
-    for (int i = 0; i < sz; i++) {
-        h[i] = f[i] * g[i];
-    }
-    mebius_div(h);
-
+    ZetaDiv<mint> zt;
+    auto h = zt.convolve(f, g);
     ans += h[1] * 2;
     std::cout << ans << '\n';
 }
