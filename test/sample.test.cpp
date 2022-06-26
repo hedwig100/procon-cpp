f05eb44ae14c5bb29206695be855219278b0780a
hedwig100 <pentagon7951@outlook.jp>
2022-06-26 19:41:05 +0900

    [add] verify-helper

---
+++ test/sample.test.cpp
@@ -0,0 +1,11 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+    return 0;
+}
