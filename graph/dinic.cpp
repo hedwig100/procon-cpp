cad8d7b8fd365910542f110130a15780884b1f80
hedwig100 <pentagon7951@outlook.jp>
2022-09-12 22:55:37 +0900

    [update] dinic

--- graph/dinic.cpp
+++ graph/dinic.cpp
@@ -1,41 +1,45 @@
 #pragma once
 #include <bits/stdc++.h>
 using namespace std;
-#include "../template/const.hpp"
 
-struct edge {
-    int to, rev;
-    int cap;
-    edge() {}
-    edge(int to, int cap, int rev) : to(to), cap(cap), rev(rev) {}
-};
-
+// Dinic
+// 最大流を求める.
+template <typename T, const T INF>
 struct Dinic {
-    int N;
+    struct edge {
+        int to, rev;
+        T cap;
+        edge() {}
+        edge(int to, T cap, int rev) : to(to), rev(rev), cap(cap) {}
+    };
+
+    int n;
     vector<vector<edge>> G;
-    vector<int> level; // min distance from s
-    vector<int> iter;
+    vector<int> level, iter;
 
-    Dinic(int _N) : N(_N) {
-        G.resize(N), level.resize(N), iter.resize(N);
+    Dinic(int n) : n(n) {
+        G.resize(n);
+        level.resize(n);
+        iter.resize(n);
     }
 
-    void add_edge(int a, int b, int c) {
-        // only directed edge
-        G[a].push_back((edge){b, c, (int)G[b].size()});
-        G[b].push_back((edge){a, 0, (int)G[a].size() - 1});
+    // add_edge
+    // aからbへ容量cの辺をはる.
+    // 制約: 0 <= a,b < n,c >= 0
+    void add_edge(int a, int b, T c) {
+        G[a].push_back(edge{b, c, (int)G[b].size()});
+        G[b].push_back(edge{a, T(0), (int)G[a].size() - 1});
     }
 
-    void _bfs(int s) {
+    void bfs(int s) {
         fill(level.begin(), level.end(), -1);
+        level[s] = 0;
         queue<int> q;
         q.push(s);
-        level[s] = 0;
         while (!q.empty()) {
             int v = q.front();
             q.pop();
-            for (int i = 0; i < (int)G[v].size(); ++i) {
-                edge &e = G[v][i];
+            for (const auto &e : G[v]) {
                 if (e.cap > 0 && level[e.to] < 0) {
                     level[e.to] = level[v] + 1;
                     q.push(e.to);
@@ -44,12 +48,12 @@ struct Dinic {
         }
     }
 
-    int _dfs(int v, int t, int f) {
+    T dfs(int v, int t, T f) {
         if (v == t) return f;
-        for (int &i = iter[v]; i < G[v].size(); ++i) {
+        for (int &i = iter[v]; i < (int)G[v].size(); i++) {
             edge &e = G[v][i];
-            if (e.cap > 0 && level[e.to] > level[v]) {
-                int d = _dfs(e.to, t, min(f, e.cap));
+            if (e.cap > 0 && level[v] < level[e.to]) {
+                T d = dfs(e.to, t, min(f, e.cap));
                 if (d > 0) {
                     e.cap -= d;
                     G[e.to][e.rev].cap += d;
@@ -60,15 +64,20 @@ struct Dinic {
         return 0;
     }
 
-    int flow(int s, int t) {
-        int flow = 0;
+    // solve
+    // sからtへの最大流を求める.
+    // 制約: 0 <= s,t < n
+    // 計算量: O(|V|^2|E|)
+    T solve(int s, int t) {
+        T flow = T(0);
         for (;;) {
-            _bfs(s);
-            if (level[t] < 0) return flow; // no augmenting path
+            bfs(s);
+            if (level[t] < 0) return flow;
             fill(iter.begin(), iter.end(), 0);
-            int f;
-            while ((f = _dfs(s, t, INF)) > 0)
+            T f;
+            while ((f = dfs(s, t, INF)) > 0) {
                 flow += f;
+            }
         }
     }
 };
---
+++ test/dinic.test.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all/GRL_6_A"
+#include "../graph/dinic.cpp"
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    Dinic<int, 100000000> din(n);
+    int a, b, c;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b >> c;
+        din.add_edge(a, b, c);
+    }
+    cout << din.solve(0, n - 1) << '\n';
+    return 0;
+}
