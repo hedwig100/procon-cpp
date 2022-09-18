8c01b3dc044fde54accca7366c2ec3b5e0b2c2c9
hedwig100 <pentagon7951@outlook.jp>
2022-09-18 15:12:22 +0900

    [add] test for geometry

---
+++ test/isp.test.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/all/CGL_1_C"
+#define ERROR 1e-9
+#include "../math/point.cpp"
+
+using P = Point<long double>;
+
+int main() {
+    cout << fixed << setprecision(20);
+
+    P p0, p1;
+    cin >> p0 >> p1;
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        P p2;
+        cin >> p2;
+        if (p0 == p2 || p1 == p2) {
+            cout << "ON_SEGMENT\n";
+            continue;
+        }
+        switch (iSP(p1, p0, p2)) {
+        case 1:
+            cout << "CLOCKWISE\n";
+            break;
+        case -1:
+            cout << "COUNTER_CLOCKWISE\n";
+            break;
+        case 2:
+            cout << "ONLINE_BACK\n";
+            break;
+        case -2:
+            cout << "ON_SEGMENT\n";
+            break;
+        case 0:
+            cout << "ONLINE_FRONT\n";
+            break;
+        }
+    }
+    return 0;
+}
---
+++ test/ss_intersection1.test.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/all/CGL_2_B"
+#define ERROR 1e-9
+#include "../math/line.cpp"
+
+using P = Point<long double>;
+using L = Line<long double>;
+using S = Segment<long double>;
+using R = Ray<long double>;
+
+int main() {
+    cout << fixed << setprecision(20);
+
+    int q;
+    cin >> q;
+    while (q--) {
+        P p0, p1, p2, p3;
+        cin >> p0 >> p1 >> p2 >> p3;
+        S s1 = S(p0, p1), s2 = S(p2, p3);
+        auto [ok, _] = ss_intersection(s1, s2);
+        cout << (ok ? 1 : 0) << '\n';
+    }
+
+    return 0;
+}
