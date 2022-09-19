e323fb5c55f822e2deb84a2a346354e6f277c612
hedwig100 <pentagon7951@outlook.jp>
2022-09-19 20:17:45 +0900

    [update] marathon_runner

--- marathon_runner.py
+++ marathon_runner.py
@@ -1,19 +1,48 @@
-import os,subprocess,argparse
+import os,subprocess,argparse,datetime
 
-def cmd(cmd_str):
-    subprocess.run(cmd_str,shell=True)
+def cmd(cmd_str,get_stdout = False):
+    if get_stdout:
+        return subprocess.check_output(cmd_str,shell=True)
+    else:
+        return subprocess.run(cmd_str,shell=True)
+
+class ResultLogger():
+    def __init__(self):
+        pass 
+
+    def log(self,scores,dirname):
+        os.mkdir(dirname)
+        self._log_score(scores,dirname)
+        self._log_src(dirname)
+
+    def _log_score(self,scores, dirname):
+        with open(f"{dirname}/res.txt",mode = "w") as f:
+            for s in scores:
+                print(s,file=f)
+
+    def _log_src(self,dirname):
+        cmd(f"cp main.cpp {dirname}/main.cpp")
 
 def runner(case_id):
     cmd(f"./main.o < tools/in/{case_id:04}.txt > tools/out/{case_id:04}.txt")
     pwd = os.getcwd()
     os.chdir("tools")
-    cmd(f"cargo run --bin vis in/{case_id:04}.txt out/{case_id:04}.txt")
+    out = cmd(f"cargo run --bin vis in/{case_id:04}.txt out/{case_id:04}.txt",get_stdout=True)
+    score = int(out[8:]) # out = "Score = 10902"
     os.chdir(pwd)
+    return score
 
 def main(args):
     cmd("make main.o")
+
+    scores = []
     for i in range(args.number):
-        runner(i)
+        scores.append(runner(i))
+    
+    dirname = f"result/{datetime.datetime.now().isoformat()}"
+    logger = ResultLogger()
+    logger.log(scores,dirname)
+
     cmd(f"open {os.getcwd()}/tools/vis.html")
 
 if __name__ == "__main__":
