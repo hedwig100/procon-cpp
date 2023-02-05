c1829f74708c169facf625cc304f498744ad68c2
hedwig100 <pentagon7951@outlook.jp>
2023-02-05 16:24:42 +0900

    [add] bell_numberにdoxgen風のコメントをつけてみる

--- math/bell_number.cpp
+++ math/bell_number.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 #include "../math/stirling_number2.cpp"
 
-// BellNumber
-// n個の区別できるものをk個の区別できない箱に0個以上に分割する方法が何通りあるか求める.
-// これをB(n,k)としてベル数という.
+/**
+ * BellNumber
+ * n個の区別できるものをk個の区別できない箱に0個以上に分割する方法が何通りあるか求める.
+ * これをB(n,k)としてベル数という.
+ */
 template <typename T>
 struct BellNumber {
     int n, k;
@@ -16,9 +18,11 @@ struct BellNumber {
         dp.assign(n + 1, vector<T>(k + 1, T(0)));
     }
 
-    // solve
-    // 0 <= i <= n,0 <= j <= kについてB(i,j)を求める.
-    // 計算量: O(nk)
+    /**
+     * solve
+     * 0 <= i <= n,0 <= j <= kについてB(i,j)を求める.
+     * 計算量: O(nk)
+     */
     T solve() {
         StirlingNumber2<T> s2(n, k);
         s2.solve();
@@ -30,8 +34,13 @@ struct BellNumber {
         return dp[n][k];
     }
 
-    // B(i,j)を返す.
-    // 制約: 0 <= i <= n, 0 <= j <= k
+    /**
+     * operator()
+     * @param i int
+     * @param j int
+     * B(i,j) を返す.
+     * 制約: 0 <= i <= n,0 <= j <= k
+     */
     T operator()(int i, int j) {
         return dp[i][j];
     }
