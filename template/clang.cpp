59583f4d0f4163af04ec55deea97c57c7030c366
hedwig100 <shota.in26@gmail.com>
2023-05-20 22:59:16 +0900

    [add] clang template

---
+++ template/clang.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cmath>
+#include <complex>
+#include <cstdint>
+#include <cstdio>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
